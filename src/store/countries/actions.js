3832101b6e988a8264c94856e05e4c3c435c435b
JennaLeysens <jennalicowley@gmail.com>
2020-07-17 16:30:24 +0200

    added a category for europe to the game

--- src/store/countries/actions.js
+++ src/store/countries/actions.js
@@ -5,9 +5,23 @@ export function storeCountryData(data) {
   return { type: "ADD_COUNTRIES", payload: data };
 }
 
+export function storeEuropeData(data) {
+  return { type: "ADD_EUROPE", payload: data };
+}
+
 export function fetchAllCountries() {
   return async (dispatch, getState) => {
     const response = await axios.get(`${COUNTRIES_API_URL}/rest/v2/all`);
     dispatch(storeCountryData(response.data));
+  };
+}
+
+export function fetchCountriesEurope() {
+  return async (dispatch, getState) => {
+    const response = await axios.get(
+      `${COUNTRIES_API_URL}/rest/v2/region/europe`
+    );
+    dispatch(storeEuropeData(response.data));
+    console.log("EUROPE", response.data);
   };
 }
